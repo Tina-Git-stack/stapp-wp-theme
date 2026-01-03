85e76f2c39a55fbe7711d1bdaff4efd14ca0f9a3
Martina Geier <geier@stapp-professional.de>
2026-01-03 20:38:22 +0100

    PHASE 5: Templates-Ordner & Custom Page Templates
    Änderungen:
    - templates/ Verzeichnis erstellt
    - templates/page-fullwidth.php erstellt:
      * Template Name: "Volle Breite"
      * Nutzt .container-wide statt .container
      * Keine Sidebar (#primary width: 100%)
      * Verwendet template-parts/content/content-page.php
    
    - assets/css/layout.css erweitert:
      * .container-wide (max-width: 1400px)
      * .fullwidth #primary (width: 100%, float: none)
    
    Custom Page Template:
    - In Page-Editor unter "Template" auswählbar
    - Volle Breite ohne Sidebar
    - Breiterer Container (1400px statt 1200px)
    
    Status: Bereit für Smoke-Test & Finale QA

---
+++ templates/page-fullwidth.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Template Name: Volle Breite
+ * Template Post Type: page
+ *
+ * @package Stapp_Theme
+ */
+
+get_header();
+?>
+
+<main id="primary" class="site-main fullwidth">
+    <div class="container-wide">
+        <?php
+        while (have_posts()) :
+            the_post();
+            get_template_part('template-parts/content/content', 'page');
+
+            if (comments_open() || get_comments_number()) :
+                comments_template();
+            endif;
+
+        endwhile;
+        ?>
+    </div>
+</main>
+
+<?php
+get_footer();
